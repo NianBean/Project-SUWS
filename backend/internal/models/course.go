ed26d89617688ce8c4029428585929c1cb03607a
NianBean <127732260+NianBean@users.noreply.github.com>
2025-04-08 21:22:13 +0700

    Update course.go

--- backend/internal/models/course.go
+++ backend/internal/models/course.go
@@ -2,11 +2,19 @@ package models
 
 type course struct {
 	//ID ใช้ int ไม่ใช่หรอ หรือป่าวอ่ะ
-	Course_ID         string `json:"-"`
-	Course_Name       string `json:"-"`
-	Course_Type       string `json:"-"`
-	Course_Instructor string `json:"-"`
-	Course_Price      int    `json:"-"`
+	Course_ID         	string 		`json:"-"`
+	Course_Name      	string 		`json:"-"`
+	Course_Description      string 		`json:"-"`
+	Course_Thumbnail_url  	string 		`json:"-"`
+	Course_Profile_url  	string 		`json:"-"`
+	Course_Duration  	string 		`json:"-"`
+	Course_Price      	float64		`json:"-"`
+	Course_Detail_url      	int    		`json:"-"`
+	Course_Rating      	float64		`json:"-"`
+	Course_Num_reviews     	int    		`json:"-"`
+	Course_Enrollment_count int    		`json:"-"`
+	Course_Created_at     	time.Time	`json:"created_at" db:"created_at"`
+	Course_Updated_at 	time.Time	`json:"updated_at" db:"updated_at"`
 	// ใสเพิ่มอีก
 }
 
